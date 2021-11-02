ff456f20c98ca5b3fe05ceaaacffaf875061cc91
Fábio Pereira <fabiopereiragt@gmail.com>
2021-11-02 12:05:01 -0300

    TP8 done.

--- src/main/java/main/ConformityRules.java
+++ src/main/java/main/ConformityRules.java
@@ -13,6 +13,9 @@ public class ConformityRules {
 
 	// Objetos DTO deve ser utilizados apenas por classes do tipo Controller.
 	public static void ra1(List<ClassData> allClass) {
+		
+		String description = "(Objetos DTO deve ser utilizados apenas "
+				+ "por classes do tipo Controller).";
 
 		List<ClassData> filterClass = 
 				allClass.stream()
@@ -31,16 +34,20 @@ public class ConformityRules {
 		});
 
 		if (classNameViolation.size() > 0) {
-			printResult(classNameViolation, "RA1");
+			printResult(classNameViolation, "RA1", description);
 			classNameViolation.clear();
 		} else {
-			System.out.println("There was no violation RA1 on this current project.");
+			System.out.println("\n- RA1: There was no violation RA1 on this current project.");
+			System.out.println(description);
 		}
 	}
 
 	// Classes DTO e Entity não devem depender de nenhuma classe específica do
 	// sistema.
 	public static void ra2(List<ClassData> allClass) {
+		
+		String description = "(Classes DTO e Entity não devem depender"
+				+ " de nenhuma classe específica do sistema).";
 
 		List<ClassData> filterClass = 
 				allClass.stream()
@@ -60,16 +67,20 @@ public class ConformityRules {
 		});
 
 		if (classNameViolation.size() > 0) {
-			printResult(classNameViolation, "RA2");
+			printResult(classNameViolation, "RA2", description);
 			classNameViolation.clear();
 		} else {
 			System.out.println("\n- RA2: There was no violation RA2 on this current project.");
+			System.out.println(description);
 		}
 	}
 
 	// As classes do pacote Repository devem ser utilizadas apenas pelos Services.
 	public static void ra3(List<ClassData> allClass) {
 
+		String description = "(As classes do pacote Repository devem "
+				+ "ser utilizadas apenas pelos Services).";
+				
 		List<ClassData> filterClass = 
 				allClass.stream()
 				.filter(x -> x.getClassType() != null)
@@ -87,16 +98,18 @@ public class ConformityRules {
 		});
 
 		if (classNameViolation.size() > 0) {
-			printResult(classNameViolation, "RA3");
+			printResult(classNameViolation, "RA3", description);
 			classNameViolation.clear();
 		} else {
 			System.out.println("\n- RA3: There was no violation RA3 on this current project.");
+			System.out.println(description);
 		}
 	}
 
 	// As classes JPA Repository devem depender apenas de Entity.
 	public static void ra4(List<ClassData> allClass) {
 
+		String description = "(As classes JPA Repository devem depender apenas de Entity).";
 		List<ClassData> filterClass = 
 				allClass.stream()
 				.filter(x -> x.getClassType() != null)
@@ -116,15 +129,18 @@ public class ConformityRules {
 		});
 
 		if (classNameViolation.size() > 0) {
-			printResult(classNameViolation, "RA4");
+			printResult(classNameViolation, "RA4", description);
 			classNameViolation.clear();
 		} else {
 			System.out.println("\n- RA4: There was no violation RA4 on this current project.");
+			System.out.println(description);
 		}
 	}
 
 	// Toda classe Entity deve implementar Serializable.
 	public static void ra5(List<ClassData> allClass) {
+		
+		String description = "(Toda classe Entity deve implementar Serializable).";
 
 		List<ClassData> filterClass = 
 				allClass.stream()
@@ -149,15 +165,17 @@ public class ConformityRules {
 		});
 
 		if (classNameViolation.size() > 0) {
-			printResult(classNameViolation, "RA5");
+			printResult(classNameViolation, "RA5", description);
 			classNameViolation.clear();
 		} else {
 			System.out.println("\n- RA5: There was no violation RA5 on this current project.");
+			System.out.println(description);
 		}
 	}
 
-	private static void printResult(List<String> classNameViolation, String violation) {
-		System.out.println("\n- " + violation + ": The classes below committed violation:\n");
+	private static void printResult(List<String> classNameViolation, String violation, String description) {
+		System.out.println("\n- " + violation + ": The classes below committed violation:");
+		System.out.println(description + "\n");
 		classNameViolation.forEach(c -> System.out.println(c));
 	}
 
